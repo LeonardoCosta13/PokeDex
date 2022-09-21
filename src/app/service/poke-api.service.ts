7e781b00abfe1dacbac4c6c1192c1da6904b24de
leonardocosta <leocostasousa123@gmail.com>
2022-09-21 14:50:57 -0300

    first commit

---
+++ src/app/service/poke-api.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+// Observable
+import { Observable } from 'rxjs';
+import { tap } from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokeApiService {
+  static apiListAllPokemons() {
+    throw new Error('Method not implemented.');
+  }
+
+  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  get apiListAllPokemons():Observable<any>{
+    return this.http.get<any>(this.url).pipe(
+      tap(res => res),
+      tap( res => {
+        console.log(res)
+      })
+    )
+  }
+}
